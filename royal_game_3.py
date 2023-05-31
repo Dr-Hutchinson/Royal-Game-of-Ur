2328f93b069252ca905732fd3962195210f6500a
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-05-30 22:28:42 -0400

    Update royal_game_3.py

--- royal_game_3.py
+++ royal_game_3.py
@@ -19,6 +19,7 @@ os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 os.environ["MAPBOX_API_KEY"] = st.secrets["mapbox_api_key"]
+mapbox_api_key = os.getenv("MAPBOX_API_KEY")
 
 with st.expander("Map showing locations discussed in this assignment."):
     #df = pd.DataFrame({
