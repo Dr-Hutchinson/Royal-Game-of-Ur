1788bdc40e94163b8ffbc7d7e8d80f63a61e92ef
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-06 13:45:51 -0400

    Update royal_game_4.py

--- royal_game_4.py
+++ royal_game_4.py
@@ -155,70 +155,70 @@ if authentication_status:
 
 
 # begin chatbot
+    with st.expander("Hopeful"):
+        if 'responses' not in st.session_state:
+            st.session_state['responses'] = ["How can I assist you?"]
 
-    if 'responses' not in st.session_state:
-        st.session_state['responses'] = ["How can I assist you?"]
+        if 'requests' not in st.session_state:
+            st.session_state['requests'] = []
 
-    if 'requests' not in st.session_state:
-        st.session_state['requests'] = []
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets["openai_api_key"])
 
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key)
+        if 'buffer_memory' not in st.session_state:
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
 
-    if 'buffer_memory' not in st.session_state:
-        st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+        system_msg_template = SystemMessagePromptTemplate.from_template(template="""You are an educational chatbot with access to various data sources on the Royal Game of Ur. When given a user question you will be supplied with information from those sources. Based on those sources, compose an insightful and accurate answer based on those sources, and cite the source of the information used in the answer.""")
 
-    system_msg_template = SystemMessagePromptTemplate.from_template(template="""You are an educational chatbot with access to various data sources on the Royal Game of Ur. When given a user question you will be supplied with information from those sources. Based on those sources, compose an insightful and accurate answer based on those sources, and cite the source of the information used in the answer.""")
+        human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
-    human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+        prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
 
-    prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+        conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
 
-    conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
-
-    # container for chat history
-    response_container = st.container()
-    # container for text box
-    textcontainer = st.container()
-
-
-    with textcontainer:
-        query = st.text_input("Query: ", key="input")
-        if query:
-            with st.spinner("typing..."):
-                conversation_string = get_conversation_string()
-                # st.code(conversation_string)
-                refined_query = query_refiner(conversation_string, query)
-                st.subheader("Refined Query:")
-                st.write(refined_query)
-                context = find_match(refined_query)
-                # print(context)
-                response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
-            st.session_state.requests.append(query)
-            st.session_state.responses.append(response)
-
-    with response_container:
-        if st.session_state['responses']:
-
-            for i in range(len(st.session_state['responses'])):
-                message(st.session_state['responses'][i],key=str(i))
-                if i < len(st.session_state['requests']):
-                    message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+        # container for chat history
+        response_container = st.container()
+        # container for text box
+        textcontainer = st.container()
 
 
-    datafile_path = "ur_source_embeddings.csv"
-    df = pd.read_csv(datafile_path)
-    df["embedding"] = df.embedding.apply(eval).apply(np.array)
-    def embeddings_search(query, df, n=3):
-            # Get the embedding of the query
-        query_embedding = get_embedding(
-            query,
-            engine="text-embedding-ada-002"
-        )
-            # Calculate cosine similarity between the query and each document
-        df["similarities"] = df.embedding.apply(lambda x: cosine_similarity(x, query_embedding))
-        # Get the top n most similar documents
-        top_n = df.sort_values("similarities", ascending=False).head(n)
-        return top_n
+        with textcontainer:
+            query = st.text_input("Query: ", key="input")
+            if query:
+                with st.spinner("typing..."):
+                    conversation_string = get_conversation_string()
+                    # st.code(conversation_string)
+                    refined_query = query_refiner(conversation_string, query)
+                    st.subheader("Refined Query:")
+                    st.write(refined_query)
+                    context = find_match(refined_query)
+                    # print(context)
+                    response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+                st.session_state.requests.append(query)
+                st.session_state.responses.append(response)
+
+        with response_container:
+            if st.session_state['responses']:
+
+                for i in range(len(st.session_state['responses'])):
+                    message(st.session_state['responses'][i],key=str(i))
+                    if i < len(st.session_state['requests']):
+                        message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+
+
+        datafile_path = "ur_source_embeddings.csv"
+        df = pd.read_csv(datafile_path)
+        df["embedding"] = df.embedding.apply(eval).apply(np.array)
+        def embeddings_search(query, df, n=3):
+                # Get the embedding of the query
+            query_embedding = get_embedding(
+                query,
+                engine="text-embedding-ada-002"
+            )
+                # Calculate cosine similarity between the query and each document
+            df["similarities"] = df.embedding.apply(lambda x: cosine_similarity(x, query_embedding))
+            # Get the top n most similar documents
+            top_n = df.sort_values("similarities", ascending=False).head(n)
+            return top_n
 
     #template = """You are an educational chatbot with access to various data sources on the Royal Game of Ur. When given a user question you will be supplied with information from those sources. Based on those sources, compose an insightful and accurate answer based on those sources, and cite the source of the information used in the answer.
     #...
