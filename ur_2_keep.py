8313d875b4bd272017b01c77f222716be7f71fcf
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-08 13:23:47 -0400

    Update ur_2_keep.py

--- ur_2_keep.py
+++ ur_2_keep.py
@@ -3,21 +3,8 @@ import streamlit as st
 #from streamlit.hashing import _CodeHasher
 import random
 import pandas as pd
-#class SessionState(object):
-    #def __init__(self, **kwargs):
-        #"""A new SessionState object."""
-        #for key, val in kwargs.items():
-            #setattr(self, key, val)
-#def get_session_state(session_id):
-    #ctx = get_report_ctx()
-    #session_infos = getattr(ctx, "session_infos", None)
-    #if session_infos is None:
-    #    session_infos = ctx.session_infos = {}
-    #if session_id not in session_infos:
-    #    session_infos[session_id] = SessionState()
-    #return session_infos[session_id]
-#session_id = _CodeHasher().to_bytes(get_report_ctx().session_id)
-#session_state = get_session_state(session_id)
+
+
 if 'board' not in st.session_state:
     st.session_state.board = [[None for _ in range(8)] for _ in range(3)]
 if 'current_player' not in st.session_state:
@@ -26,15 +13,7 @@ if 'pieces' not in st.session_state:
     st.session_state.pieces = {1: [None]*7, 2: [None]*7}
 if 'game_over' not in st.session_state:
     st.session_state.game_over = False
-#if not hasattr(session_state, 'board'):
-    #session_state.board = [[None for _ in range(8)] for _ in range(3)]
-#if not hasattr(session_state, 'current_player'):
-    #session_state.current_player = 1  # 1 for player 1, 2 for player 2
-#if not hasattr(session_state, 'pieces'):
-    # Initialize the pieces for each player. Each player has 7 pieces.
-    #session_state.pieces = {1: [None]*7, 2: [None]*7}
-#if not hasattr(session_state, 'game_over'):
-    #session_state.game_over = False
+
 def draw_board():
     # Create a DataFrame to represent the game board
     board_df = pd.DataFrame(st.session_state.board)
@@ -44,6 +23,7 @@ def draw_board():
     board_df = board_df.replace({None: "□", 0: "□", 1: "🔴", 2: "🔵"})
     # Display the DataFrame in Streamlit
     st.table(board_df)
+
 def initialize_game():
     # Initialize game state variables
     st.session_state.board = [[0]*8 for _ in range(3)]
@@ -58,18 +38,11 @@ def initialize_game():
     # Set up GUI
     st.title("The Royal Game of Ur")
     st.markdown("Welcome to the Royal Game of Ur! Player 1's turn.")
+
 # Call the function at the start of our script
 if 'board' not in st.session_state.board:
     initialize_game()
-#def draw_board():
-    # Create a DataFrame to represent the game board
-    #board_df = pd.DataFrame(st.session_state.board)
-    # Convert the DataFrame's data type to object
-    #board_df = board_df.astype(object)
-    # Replace the values in the DataFrame with emojis
-    #board_df = board_df.replace({None: "□", 0: "□", 1: "🔴", 2: "🔵"})
-    # Display the DataFrame in Streamlit
-    #st.table(board_df)
+
 class Game:
     def __init__(self):
         self.board = [[0]*8 for _ in range(3)]
@@ -84,6 +57,7 @@ class Game:
         self.whiteturn = True
         self.wpath = [-1, 11, 8, 5, 2, 1, 4, 7, 10, 12, 13, 15, 14, 17, 18, 19, 16, 99]  # Define the path for white pieces here
         self.bpath = [-1, 9, 6, 3, 0, 1, 4, 7, 10, 12, 13, 15, 16, 19, 18, 17, 14, 99]  # Define the path for black pieces here
+
     def throw_dice(self):
         self.dice = random.randint(0, 4)
         st.session_state.dice = self.dice
@@ -94,8 +68,10 @@ class Game:
             if self.turn == 2:
                 self.ai_move()
             self.change_turn()  # Move this line here
+
     def select_piece(self, piece):
         self.selected_piece = piece - 1  # Subtract 1 to adjust for 0-based indexing
+
     def show_moves(self):
         possible_moves = []
         for i in range(1, self.dice + 1):
@@ -109,6 +85,7 @@ class Game:
                         possible_moves.append(target_square)
             # Display the possible moves to the user
         st.write(f"Possible moves for piece {self.selected_piece}: {possible_moves}")
+
     def available_squares(self, piece):
         available_squares = []
         current_position = self.fishki_positions[self.turn-1][piece]
@@ -122,6 +99,7 @@ class Game:
                 if self.board[row][col] != self.turn:
                     available_squares.append(target_square)
         return available_squares
+
     def move_piece(self, stone, steps):
         stone = stone - 1
         st.write(f"Moving piece {stone} by {steps} steps")  # Debugging statement
@@ -176,8 +154,10 @@ class Game:
         st.write(f"Updated fishki_positions: {self.fishki_positions}")
         # Update st.session_state.board
         st.session_state.board = board_df.values.tolist()
+
     def change_turn(self):
         self.turn = 3 - self.turn
+
     def find_winner(self):
         # Check if all pieces of a player have reached the end of the game board
         if self.fishki_positions[0].count(99) == 7:
@@ -228,6 +208,9 @@ class Game:
 
             # Print a debugging statement
             st.write(f"AI moved piece {piece} to square {square}")
+            st.write(f"Updated game board: {board_df}")
+            st.write(f"Updated fishki_positions: {self.fishki_positions}")
+            st.session_state.board = board_df.values.tolist()
 
         # If no move was found, skip the turn
         else:
@@ -238,11 +221,15 @@ class Game:
 def main():
     if 'game' not in st.session_state:
         st.session_state.game = Game()
+
     game = st.session_state.game
+
     draw_board()
+
     if st.button("Throw Dice"):
         game.throw_dice()
         st.write(f"You rolled a {game.dice}.")
+
     if game.dice != 0:
         with st.form(key='my_form'):
             available_pieces = [i for i in range(7) if game.fishki_positions[game.turn-1][i] is not None]
@@ -255,9 +242,11 @@ def main():
             game.move_piece(game.selected_piece, game.dice)
             st.session_state.game = game
             draw_board()
+
     if game.turn == 2:
         game.ai_move()
         draw_board()
+
     if game.find_winner():
         st.write(f"Player {game.winner} wins!")
         st.stop()
