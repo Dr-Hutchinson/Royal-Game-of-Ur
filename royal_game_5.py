f0cdebc30e88b6d76dd33eb038a393acc359348f
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-10 17:49:51 -0400

    Update royal_game_5.py

--- royal_game_5.py
+++ royal_game_5.py
@@ -343,7 +343,7 @@ Once you're done asking questions and learning from the chatbot, answer the ques
                             #response = conversation.predict(input=f"Query:\n{query}\n\nContext:\n {conversation_string}"))
                             response, tokens = count_tokens(conversation, f"Query:\n{query}\n\nContext:\n {conversation_string}")
                             token_counts.append(tokens)
-                            st.write("Token Count= " + token_counts)
+                            st.write("Token Count= " + str(token_counts))
 
                             # Convert the "unnamed:" column values into a list
                             #source_rows = results_df["Unnamed:0"].tolist()
