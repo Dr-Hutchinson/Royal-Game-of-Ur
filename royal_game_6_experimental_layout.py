c5e33d0fcc63d05fa46de83310c388365d291a5f
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-17 18:06:25 -0400

    Update royal_game_6_experimental_layout.py

--- royal_game_6_experimental_layout.py
+++ royal_game_6_experimental_layout.py
@@ -179,7 +179,7 @@ if authentication_status:
 
     with st.expander("Earth Engine Test"):
 
-        #ee.Authenticate()
+        ee.Authenticate()
         ee.Initialize()
         st.image(ee.Image("NASA/NASADEM_HGT/001").get("title").getInfo())
 
