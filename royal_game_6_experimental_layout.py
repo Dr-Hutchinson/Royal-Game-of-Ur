6ecf2faba535da3b94e0ac5af8c24554de1244af
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-18 13:18:50 -0400

    Update royal_game_6_experimental_layout.py

--- royal_game_6_experimental_layout.py
+++ royal_game_6_experimental_layout.py
@@ -192,11 +192,6 @@ if authentication_status:
 # begin code
         ee.Initialize(credentials2)
 
-        # Check if 'Map' already exists in session_state
-        # If not, then initialize it
-        #if 'folium_map' not in st.session_state:
-        #    st.session_state['folium_map'] = folium.Map()
-
         def add_ee_layer(self, ee_image_object, vis_params, name):
             """Adds a method for displaying Earth Engine image tiles to  folium map."""
             map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
@@ -221,20 +216,24 @@ if authentication_status:
             lc_img = lc.select('LC_Type1').filterDate(i_date).first()
             return lc_img
 
-        folium_map = folium.Map()
+        @st.experimental_memo
+        def create_map():
+            # Create a new folium map.
+            folium_map = folium.Map()
 
-        # Add the Earth Engine layer to the map.
-        vis_params = {
-            'min': 0,
-            'max': 17,
-            'palette': ['aec3d4', '152106', '225129', '369b47', '30eb5b', '387242', '6a2325', 'c3aa69', 'b76031', 'd9903d', '91af40', '111149'],
-            'bands': ['LC_Type1']
-        }
+            # Add the Earth Engine layer to the map.
+            vis_params = {
+                'min': 0,
+                'max': 17,
+                'palette': ['aec3d4', '152106', '225129', '369b47', '30eb5b', '387242', '6a2325', 'c3aa69', 'b76031', 'd9903d', '91af40', '111149'],
+                'bands': ['LC_Type1']
+            }
+            folium_map.add_ee_layer(load_data(), vis_params, 'MODIS Land Cover')
 
-        folium_map.add_ee_layer(load_data(), vis_params, 'MODIS Land Cover')
+            return folium_map
 
-        rendered_map = st_folium(folium_map)
-        # end code
+        rendered_map = st_folium(create_map())
+                # end code
 
         # end code
 
