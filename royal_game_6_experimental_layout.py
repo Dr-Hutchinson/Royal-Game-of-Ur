f6630f452ac6f8d8bee68e4dd6c75ebec0db9eb9
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-20 12:44:00 -0400

    Update royal_game_6_experimental_layout.py

--- royal_game_6_experimental_layout.py
+++ royal_game_6_experimental_layout.py
@@ -567,30 +567,32 @@ if authentication_status:
                         if submit_button and query is not None and query != "":
                             with st.spinner("Getting Response..."):
 
-                                #conversation_string = get_conversation_string()
+                                conversation_string = get_conversation_string()
 
                                 # original code for running chat dialogue, DON'T DELETE
-                                #response, tokens = count_tokens(conversation, f"""{query}\n""")
+                                response, tokens = count_tokens(conversation, f"""{query}\n""")
 
                                 # attempts to implement GPT functions
                                 #response, tokens = agent.run(f"""{query}\n""")
                                 #response = agent.run(f"""{query}\n""")
                                 #st.write(response)
 
-                                conversation_string = get_conversation_string()
+                                # Multi-option chat thread for tools implementation - don't delete
+
+                                #conversation_string = get_conversation_string()
 
                                 # Check if the user input is a command
-                                if query.startswith("/"):
+                                #if query.startswith("/"):
                                     # Check if the command is "/start"
-                                    if query == "/start":
+                                #    if query == "/start":
                                         # Dialogue option 0 - for starting chat dialogue
-                                        response, tokens = count_tokens(conversation, f"""{query}\n""")
-                                    else:
+                                #        response, tokens = count_tokens(conversation, f"""{query}\n""")
+                                #    else:
                                         # Dialogue option 2 - for command uses
-                                        response = agent.run(f"""{query}\n""")
-                                else:
+                                #        response = agent.run(f"""{query}\n""")
+                                #else:
                                     # Dialogue option 1 - for normal Q&A interactions with chatbot
-                                    response, tokens = count_tokens(conversation, f"""{query}\n\n<begin User-Response-Evaluation mode>\nInitial Thought:Thought:>""")
+                                #    response, tokens = count_tokens(conversation, f"""{query}\n\n<begin User-Response-Evaluation mode>\nInitial Thought:Thought:>""")
 
                                 st.write("Token Count= " + str(st.session_state.token_count))
 
