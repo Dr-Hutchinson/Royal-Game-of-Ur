983bcbc935b5d183d5a564156c52395c7b5adf6c
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-21 15:18:47 -0400

    Update royal_game_6_experimental_layout.py

--- royal_game_6_experimental_layout.py
+++ royal_game_6_experimental_layout.py
@@ -368,6 +368,9 @@ if authentication_status:
                 if 'requests' not in st.session_state:
                     st.session_state['requests'] = []
 
+                if 'sources' not in st.session_state:
+                    st.session_state['sources'] = []
+
                 llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", openai_api_key=st.secrets["openai_api_key"])
 
                 if 'buffer_memory' not in st.session_state:
