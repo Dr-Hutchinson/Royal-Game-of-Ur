c24bd7d831f42fc136ec93b305ccedac2ead373e
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-21 21:40:45 -0400

    Update royal_game_6_experimental_layout.py

--- royal_game_6_experimental_layout.py
+++ royal_game_6_experimental_layout.py
@@ -632,7 +632,7 @@ if authentication_status:
                                     # Step 3: Execute Pull Row function
                                     learning_objectives, question, answer = Pull_Row(sh_questions)
                                     st.session_state.sources.append((learning_objectives, question, answer))
-                                    st.session_state.responses.append(f"Bot: \n\nQuestion {st.session_state['question_number']}: \n\n\nLearning Objectives: {learning_objectives}\n\nQuestion: {question}\n\n")
+                                    st.session_state.responses.append(f"Bot: \n\nQuestion {st.session_state['question_number']}: \n\nLearning Objectives: {learning_objectives}\n\nQuestion: {question}\n\n")
                                     st.session_state['question_number'] += 1
                                     #conversation_string = get_conversation_string()
                                     #st.write(conversation_string)
@@ -650,7 +650,7 @@ if authentication_status:
                                         #Upload_Data(sh_scores, user, 5)
                                         learning_objectives, question, answer = Pull_Row(sh_questions)
                                         st.session_state.sources.append((learning_objectives, question, answer))
-                                        st.session_state.responses.append(f"Learning Objectives: {learning_objectives}\n\nQuestion: {question}")
+                                        st.session_state.responses.append(f"Bot: \n\nQuestion {st.session_state['question_number']}: \n\nLearning Objectives: {learning_objectives}\n\nQuestion: {question}\n\n")
                                         st.session_state['question_number'] += 1
                                         st.write(conversation_string)
                                     elif "Partial" in response:
