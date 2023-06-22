665bca0935a03c63df898043ce8380e9b36a65e9
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-22 19:08:47 -0400

    Update royal_game_6_experimental_layout.py

--- royal_game_6_experimental_layout.py
+++ royal_game_6_experimental_layout.py
@@ -383,7 +383,8 @@ if authentication_status:
                     st.session_state['answer'] = None
 
                 if 'conversation' not in st.session_state:
-                    st.session_state['conversation'] = ["Hello, I'm Clio! Enter /start to begin the assignment!"]
+                    st.session_state['conversation'] = [('Bot', "Hello, I'm Clio! Enter /start to begin the assignment!")]
+
 
                 llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", openai_api_key=st.secrets["openai_api_key"])
 
