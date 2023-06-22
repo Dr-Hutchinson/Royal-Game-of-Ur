901b7b15f42c9b6cf17661a3e3cfb8ae856cf1ea
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-22 19:01:02 -0400

    Update royal_game_6_experimental_layout.py

--- royal_game_6_experimental_layout.py
+++ royal_game_6_experimental_layout.py
@@ -382,8 +382,8 @@ if authentication_status:
                 if 'answer' not in st.session_state:
                     st.session_state['answer'] = None
 
-                if 'answer' not in st.session_state:
-                    st.session_state['conversation'] = [] 
+                if 'conversation' not in st.session_state:
+                    st.session_state['conversation'] = []
 
                 llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", openai_api_key=st.secrets["openai_api_key"])
 
