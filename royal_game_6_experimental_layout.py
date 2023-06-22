30b20efa5331fb30488141facbb2f93ba70bae18
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-22 13:56:35 -0400

    Update royal_game_6_experimental_layout.py

--- royal_game_6_experimental_layout.py
+++ royal_game_6_experimental_layout.py
@@ -722,11 +722,12 @@ if authentication_status:
                 #                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
 
                 with response_container:
-                    for i, question in enumerate(reversed(st.session_state['questions'])):
-                        message(question, key=str(i))
-                        if i < len(st.session_state['requests']):
-                            message(st.session_state["requests"][-i-1], is_user=True, key=str(i) + '_user')
-                            message(st.session_state['responses'][-i-1], key=str(i) + '_bot')
+                    if st.session_state['responses']:
+                        for i, question in enumerate(reversed(st.session_state['questions'])):
+                            message(question, key=str(i))
+                            if i < len(st.session_state['requests']):
+                                message(st.session_state["requests"][-i-1], is_user=True, key=str(i) + '_user')
+                                message(st.session_state['responses'][-i-1], key=str(i) + '_bot')
 
 
 
