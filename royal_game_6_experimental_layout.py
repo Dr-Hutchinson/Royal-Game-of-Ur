823a50562b543d94eb34ebd430e6c2ae3d5e0f95
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-06-22 14:33:15 -0400

    Update royal_game_6_experimental_layout.py

--- royal_game_6_experimental_layout.py
+++ royal_game_6_experimental_layout.py
@@ -683,13 +683,13 @@ if authentication_status:
                                     #st.write("Condition: Accurate")
                                     #st.write(conversation_string)
                                     # Step 6: Interpret LLM output
-                                    if re.search(r'\bPartially Accurate\b', response):
+                                    if re.search(r'\bPartially Accurate\b', response) or re.search(r'\bpartially accurate\b', response):
                                         #st.session_state.requests.append(query)
                                         # Partial credit, continue question/conversation
                                         #Upload_Data(sh_scores, user, 3)
                                         st.write("Condition: Partial")
                                         st.write(conversation_string)
-                                    elif re.search(r'\bAccurate\b', response) or re.search(r'\bLet’s now move on to the next question\b', response):
+                                    elif re.search(r'\bAccurate\b', response) or re.search(r'\baccurate\b', response) or re.search(r'\bLet’s now move on to the next question\b', response) or re.search(r'\bLet’s move on to the next question\b', response):
                                         #st.session_state.requests.append(query)
                                         # Full credit, next question
                                         #Upload_Data(sh_scores, user, 5)
